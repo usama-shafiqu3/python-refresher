5aced06015c300d3c1a6a2248d64c7b54b2457bc
usama-shafiqu3 <53015961+usama-shafiqu3@users.noreply.github.com>
2020-06-21 11:04:50 +0500

    UPdated Scripts

--- Decorator.py
+++ Decorator.py
@@ -1,3 +1,4 @@
+import datetime
 
 class Header:
 
@@ -5,3 +6,17 @@ class Header:
 
          print("::::::::::::::::::::::::Alphatuts++:::::::::::::::::::::::")
          print("::::::::::::::: Let's Learn Coding Together ::::::::::::::")
+
+    def info(self):
+
+         print("\n\n\n        Author : Usama Shafique")
+         print("        Github : usamashafqiu3 ")
+         print("     Software Engineer | enterprenurer | Freelancer")
+         print("      SOFTWARE ENGINEERING DEPARTMENT")
+         print(" ARMY PUBLIC COLLEGE OF MANAGMENT & SCIENCES")
+         print("Visit My Portfolio :https://github.com/usama-shafiqu3")
+
+    def data_time(self):
+
+        PST = datetime.datetime.now()
+        print(PST.strftime("%A, %m %B, %Y %I:%M:%S %p"))
--- Python-refresher_0.1.py
+++ Python-refresher_0.1.py
@@ -1,12 +1,16 @@
 """                                   Python Refresher-0.1                  """
 import os
 from DATA import CL_DATA
-
+from Decorator import Header
 
 main_data = CL_DATA
-
+dsply = Header
 main_data.welcome_note('self')
 
 os.system('cls')
 
 main_data.dsply_data('self')
+
+dsply.info('self')
+
+dsply.data_time('self')
